d7727d4e0e5fa333adcce6d5ab2fabe08945587d
Joey <joey@lineageos.org>
2019-01-23 17:49:25 +0100

    Launcher3: implement hidden & protected apps
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: programminghoch10 <hoch10@kabelbw.de>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -68,6 +68,8 @@ import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.qsb.QsbContainerView;
+import com.android.launcher3.lineage.LineageUtils;
+import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
@@ -99,6 +101,8 @@ public class SettingsActivity extends FragmentActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -391,6 +395,16 @@ public class SettingsActivity extends FragmentActivity
                 case Utilities.KEY_MINUS_ONE:
                     mShowGoogleAppPref = preference;
                     updateIsGoogleAppEnabled();
+                    return true;
+                case KEY_TRUST_APPS:
+                    preference.setOnPreferenceClickListener(p -> {
+                        LineageUtils.showLockScreen(getActivity(),
+                                getString(R.string.trust_apps_manager_name), () -> {
+                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                            startActivity(intent);
+                        });
+                        return true;
+                    });
                     return true;
             }
 
